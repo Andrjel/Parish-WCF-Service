1199a19bfa96bd53cd95a741ede4edaf9736ce0d
Marcin Kazmierczak <km48913@zut.edu.pl>
2024-06-09 15:36:16 +0200

    Dodaj pliki projektów.

---
+++ Parish/IParishService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Parish
+{
+    [ServiceContract]
+    public interface IParishService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "v1/parishes", RequestFormat = WebMessageFormat.Json)]
+        List<Parish> GetParishes();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "v1/parishes/{id}", RequestFormat = WebMessageFormat.Json)]
+        Parish GetParish(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "v1/parishes", Method = "POST", RequestFormat = WebMessageFormat.Json)]
+        void AddParish(Parish parish);
+    }
+}
